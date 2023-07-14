1996d9ed8be09eccf05969789b282f648bbf94c6
Sreejith M <108933441+SreejithgithubM@users.noreply.github.com>
2023-07-14 12:25:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var profileMenu = document.getElementById("profileMenu");
+
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu")
+}
+
+let sidebarActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity(){
+    sidebarActivity.classList.toggle("open-activity")
+
+    if(sidebarActivity.classList.contains("open-activity")){
+        moreLink.innerHTML  = "Show Less <b>-</b>"
+    }else{
+        moreLink.innerHTML  = "Show more <b>+</b>"
+    }
+}
